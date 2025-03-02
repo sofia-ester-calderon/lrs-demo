c1cd8209cc95668b95d2d6754d44a37d0e679132
Sofia Calderon <sofia.ester.calderon@gmail.com>
2025-03-03 10:44:07 +1100

    add correct types

--- packages/ui/src/headerText.tsx
+++ packages/ui/src/headerText.tsx
@@ -1,15 +1,23 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 
-export const HeaderText = (params: any) => {
+export const HeaderText = ({ blok }: HeaderTextProps) => {
   return (
     <section
-      {...storyblokEditable(params.blok)}
+      {...storyblokEditable(blok)}
       className="ui-group ui-rounded-lg ui-border "
     >
       <h1 className="ui-mb-3 ui-text-2xl ui-font-semibold">
-        {params.blok.headline}
+        {blok.headline}
       </h1>
-      <p className="text-center text-xl mt-8">{params.blok.content}</p>
+      <p className="text-center text-xl mt-8">{blok.content}</p>
     </section>
   );
 };
+
+interface HeaderTextProps {
+  blok: {
+    headline: string,
+    content: string,
+  }
+}
+
--- packages/ui/src/landingPage.tsx
+++ packages/ui/src/landingPage.tsx
@@ -1,14 +1,19 @@
 import {
+    SbBlokData,
     StoryblokServerComponent,
     storyblokEditable,
 } from "@storyblok/react/rsc";
 
-export const landingPage = (params: any) => {
+export const landingPage = ({ blok }: LandingPageProps) => {
     return (
-        <main {...storyblokEditable(params.blok)}>
-            {params.blok.blocks.map((blok: any) => (
+        <main {...storyblokEditable(blok)}>
+            {Array.isArray(blok.blocks) && blok.blocks?.map((blok: SbBlokData) => (
                 <StoryblokServerComponent blok={blok} key={blok._uid} />
             ))}
         </main>
     );
 };
+
+interface LandingPageProps {
+    blok: SbBlokData,
+}
